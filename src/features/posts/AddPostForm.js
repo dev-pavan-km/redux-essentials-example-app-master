8b12928edfd9125a5564a310967affa451b0dc37
dev.pavan.km <dev.pavan.km@gmail.com>
2024-02-04 15:27:27 +0530

    refactor: rtk query basics

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import { addNewPost } from './postsSlice'
 import { selectAllUsers } from '../users/usersSlice'
+import { useAddNewPostMutation } from '../api/apiSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
@@ -10,31 +11,34 @@ export const AddPostForm = () => {
   const [userId, setUserId] = useState('')
   const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
+  const [addNewPost, { isLoading }] = useAddNewPostMutation()
   const users = useSelector(selectAllUsers)
 
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
   const onAuthorChanged = (e) => setUserId(e.target.value)
 
+  // const canSave =
+  //   [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
+  const canSave = [title, content, userId].every(Boolean) && !isLoading
+
   const onSavePostClicked = async () => {
     if (canSave) {
       try {
-        setAddRequestStatus('pending')
-        await dispatch(addNewPost({ title, content, user: userId })).unwrap()
+        // setAddRequestStatus('pending')
+        // await dispatch(addNewPost({ title, content, user: userId })).unwrap()
+        await addNewPost({ title, content, user: userId }).unwrap()
         setTitle('')
         setContent('')
         setUserId('')
       } catch (err) {
         console.error('Failed to save the post: ', err)
       } finally {
-        setAddRequestStatus('idle')
+        // setAddRequestStatus('idle')
       }
     }
   }
-
-  const canSave =
-    [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
 
   const usersOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
@@ -11,9 +11,12 @@ import {
   selectPostById,
 } from './postsSlice'
 import { Spinner } from '../../components/Spinner'
+import classnames from 'classnames'
 
-let PostExcerpt = ({ postId }) => {
-  const post = useSelector((state) => selectPostById(state, postId))
+import { useGetPostsQuery } from '../api/apiSlice'
+
+let PostExcerpt = ({ post }) => {
+  // const post = useSelector((state) => selectPostById(state, postId))
 
   return (
     <article className="post-excerpt">
@@ -35,33 +38,71 @@ let PostExcerpt = ({ postId }) => {
 PostExcerpt = React.memo(PostExcerpt)
 
 export const PostsList = () => {
-  const dispatch = useDispatch()
-  const orderedPostIds = useSelector(selectPostIds)
-  const posts = useSelector(selectAllPosts)
-  const postStatus = useSelector((state) => state.posts.status)
-  const error = useSelector((state) => state.posts.error)
+  // const dispatch = useDispatch()
+  // const orderedPostIds = useSelector(selectPostIds)
+  // const posts = useSelector(selectAllPosts)
+  // const postStatus = useSelector((state) => state.posts.status)
+  // const error = useSelector((state) => state.posts.error)
 
-  useEffect(() => {
-    if (postStatus === 'idle') {
-      dispatch(fetchPosts())
-    }
-  }, [postStatus, dispatch])
+  // useEffect(() => {
+  //   if (postStatus === 'idle') {
+  //     dispatch(fetchPosts())
+  //   }
+  // }, [postStatus, dispatch])
+
+  const {
+    data: posts,
+    isLoading,
+    isFetching,
+    isSuccess,
+    isError,
+    error,
+    refetch,
+  } = useGetPostsQuery()
+
+  const sortedPosts = useMemo(() => {
+    const sortedPosts = (posts || []).slice()
+    // Sort posts in descending chronological order
+    sortedPosts.sort((a, b) => b.date.localeCompare(a.date))
+    return sortedPosts
+  }, [posts])
 
   let content
 
-  if (postStatus === 'loading') {
+  // if (postStatus === 'loading') {
+  //   content = <Spinner text="Loading..." />
+  // } else if (postStatus === 'succeeded') {
+  //   content = orderedPostIds.map((postId) => (
+  //     <PostExcerpt key={postId} postId={postId} />
+  //   ))
+  // } else if (postStatus === 'failed') {
+  //   content = <div>{error}</div>
+  // }
+
+  if (isLoading) {
     content = <Spinner text="Loading..." />
-  } else if (postStatus === 'succeeded') {
-    content = orderedPostIds.map((postId) => (
-      <PostExcerpt key={postId} postId={postId} />
+  } else if (isSuccess) {
+    // content = posts.map((post) => <PostExcerpt key={post.id} post={post} />)
+    // content = sortedPosts.map((post) => (
+    //   <PostExcerpt key={post.id} post={post} />
+    // ))
+    const renderedPosts = sortedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
     ))
-  } else if (postStatus === 'failed') {
-    content = <div>{error}</div>
+
+    const containerClassname = classnames('posts-container', {
+      disabled: isFetching,
+    })
+
+    content = <div className={containerClassname}>{renderedPosts}</div>
+  } else if (isError) {
+    content = <div>{error.toString()}</div>
   }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
+      <button onClick={refetch}>Refetch Posts</button>
       {content}
     </section>
   )
